d45cba556eb06fd6ee878bdbef40eda15f0e29c8
Carol St.Louis <1.selva.oscura@gmail.com>
2016-11-18 15:22:26 -0800

    disaggregated board creation into createBaseRow and createBasicBoard functions

--- src/App.js
+++ src/App.js
@@ -4,16 +4,27 @@ import './App.css';
 const App = React.createClass({
   getInitialState(){
     let board = [];
-    let row = [];
-    let numbers = [];
-    for(let i = 1; i<=9; i++){
-      numbers.push(i);
+    board = this.createBoard();
+    board.forEach((row)=>{
+      console.log(row.join(' '));
+    });
+    return{
+      board:      board,
+      messages:   "",
     }
+  },
+  createBaseRow(){
+    let row = [];
+    let numbers = [1,2,3,4,5,6,7,8,9];
     while(numbers.length>0){
       let n =Math.floor(Math.random()*numbers.length);
       row.push(numbers[n]);
       numbers=numbers.slice(0,n).concat(numbers.slice(n+1, numbers.length));
     }
+    return row;
+  },
+  createBasicBoard(row){
+    let board = [];
     board.push(row);
     board.push(row.slice(3,9).concat(row.slice(0,3)));
     board.push(row.slice(6,9).concat(row.slice(0,6)));
@@ -23,13 +34,12 @@ const App = React.createClass({
     board.push(row.slice(2,9).concat(row.slice(0,2)));
     board.push(row.slice(5,9).concat(row.slice(0,5)));
     board.push(row.slice(8,9).concat(row.slice(0,8)));
-    board.forEach((row)=>{
-      console.log(row.join(' '));
-    });
-    return{
-      board:      board,
-      messages:   "",
-    }
+    return board;
+  },
+  createBoard(){
+    let row = this.createBaseRow();
+    let board = this.createBasicBoard(row);
+    return board;
   },
   render() {
     console.log("state", this.state);
