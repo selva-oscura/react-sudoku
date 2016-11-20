ae525df173026c15f1f171f4332057b735ea3fc9
Carol St.Louis <1.selva.oscura@gmail.com>
2016-11-19 16:38:45 -0800

    removed console.log; imported and displayed Square Component after passing rowNumber, colNumber, and values to it

--- src/Row.js
+++ src/Row.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Square from './Square';
 import './Row.css';
 
-const Row = ({row}) => {
-	console.log('row', row);
+const Row = ({rowNumber, rowData}) => {
+	const squares = rowData.map((value, i)=>(
+		<Square 
+			key={i} 
+			rowNumber={rowNumber} 
+			colNumber={i+1} 
+			value={value}
+		 />
+	));
 	return(
 		<div className="row">
+			{squares}			
 		</div>
 	)
 }
