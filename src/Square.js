deca1ae36ccf2e7d3c84bfa51914e2e32dbb65e4
Carol St.Louis <1.selva.oscura@gmail.com>
2016-11-26 22:26:42 -0800

    added css and row/col-dependent classes to display darker borders for board and 3x3 squares

--- src/Square.js
+++ src/Square.js
@@ -2,9 +2,28 @@ import React from 'react';
 import './Square.css';
 
 const Square = ({rowNumber, colNumber, value}) => {
+	let squareStyle="square";
+	if(rowNumber===1){
+		squareStyle+=" board-top";
+	}else if(rowNumber===9){
+		squareStyle+=" board-bottom"
+	}else if(rowNumber===3 || rowNumber===6){
+		squareStyle+=" row-divider-bottom";
+	}else if(rowNumber===4 || rowNumber===7){
+		squareStyle+=" row-divider-top";
+	}
+	if(colNumber===1){
+		squareStyle+=" board-left";
+	}else if(colNumber===9){
+		squareStyle+=" board-right"
+	}else if(colNumber===3 || colNumber===6){
+		squareStyle+=" row-divider-right";
+	}else if(colNumber===4 || colNumber===7){
+		squareStyle+=" row-divider-left";
+	}
 
 	return (
-		<div className="square">
+		<div className={squareStyle}>
 			<p>{value}</p>
 		</div>
 	)
