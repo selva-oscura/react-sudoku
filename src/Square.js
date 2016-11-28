bec368889d7d8ad358a38e9b5b7adcdd09f10b92
Carol St.Louis <1.selva.oscura@gmail.com>
2016-11-27 20:39:41 -0800

    conditional to add 'selected' to style if square.selected is true

--- src/Square.js
+++ src/Square.js
@@ -4,22 +4,25 @@ import './Square.css';
 const Square = ({rowNumber, colNumber, square, selectSquare}) => {
 	let squareStyle="square";
 	if(rowNumber===1){
-		squareStyle+=" board-top";
+		squareStyle += " board-top";
 	}else if(rowNumber===9){
-		squareStyle+=" board-bottom"
+		squareStyle += " board-bottom"
 	}else if(rowNumber===3 || rowNumber===6){
-		squareStyle+=" row-divider-bottom";
+		squareStyle += " row-divider-bottom";
 	}else if(rowNumber===4 || rowNumber===7){
-		squareStyle+=" row-divider-top";
+		squareStyle += " row-divider-top";
 	}
 	if(colNumber===1){
-		squareStyle+=" board-left";
+		squareStyle += " board-left";
 	}else if(colNumber===9){
-		squareStyle+=" board-right"
+		squareStyle += " board-right"
 	}else if(colNumber===3 || colNumber===6){
-		squareStyle+=" row-divider-right";
+		squareStyle += " row-divider-right";
 	}else if(colNumber===4 || colNumber===7){
-		squareStyle+=" row-divider-left";
+		squareStyle += " row-divider-left";
+	}
+	if(square.selected){
+		squareStyle += " selected";
 	}
 	return (
 		<div 
