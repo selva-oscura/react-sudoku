644e4801717c5fcd0283dcc2f0ccd0f4dbbc799c
Carol St.Louis <1.selva.oscura@gmail.com>
2016-11-27 19:02:56 -0800

    added pass-through of selectSquare to connect App Component's selectSquare function via Row to Square Component's onClick event handler

--- src/Board.js
+++ src/Board.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Row from './Row';
 import './Board.css';
 
-const Board = ({board}) => {
+const Board = ({board, selectSquare}) => {
 	const rows = board.map((row, i) => (
 		<Row 
 			key={i} 
 			rowNumber={i+1} 
 			rowData={row} 
+			selectSquare={selectSquare}
 		/>
 	));
 	return (
