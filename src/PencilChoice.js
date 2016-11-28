77a68ea9f1e5f67a7b3d321ec7dffec0261dc115
Carol St.Louis <1.selva.oscura@gmail.com>
2016-11-27 19:18:09 -0800

    created and imported css for PencilChoice and PencilChoices Components

--- src/PencilChoice.js
+++ src/PencilChoice.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './PencilChoice.css';
 
 const PencilChoice = ({pencilChoice, updatePencilMarks}) => {
 	return (
--- src/PencilChoices.js
+++ src/PencilChoices.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './PencilChoices.css';
 import PencilChoice from './PencilChoice';
 
 const PencilChoices = ({updatePencilMarks}) => {
