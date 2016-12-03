b85a2ded695436b67ec694226c7b37fb6af64cea
Carol St.Louis <1.selva.oscura@gmail.com>
2016-12-02 23:55:55 -0800

    added display trigger for NewGame component; added passing of gameStatus to Message from App and passing newGame trigger from NewGame to App

--- src/Message.js
+++ src/Message.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import NewGame from './NewGame';
 import './Message.css';
 
-const Message = ({message}) => {
+const Message = ({message, gameStatus, newGame}) => {
 	let messageStyle="";
 	if(message.length>0){
 		messageStyle="message";
 	}
+	if(gameStatus==="gameOver"){
+
+	}
 	return (
 		<div className={messageStyle}>
 			<p>{message}</p>
+			{ gameStatus==="gameOver" ? <NewGame newGame={newGame} /> : null }
 		</div>
 	)
 }
