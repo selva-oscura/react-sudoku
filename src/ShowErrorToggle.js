f47193ec1e9f9726ec47f2a4eb1fe9a9a3151d2f
Carol St.Louis <1.selva.oscura@gmail.com>
2016-12-05 23:56:44 -0800

    added div around checkbox

--- src/ShowErrorToggle.js
+++ src/ShowErrorToggle.js
@@ -2,16 +2,18 @@ import React from 'react';
 
 const ShowErrorToggle = ({showErrors, toggleShowErrors}) => {
 	return (
-		<label htmlFor="show-errors">Show Errors
-			<input 
-				type="checkbox" 
-				name="show-errors"
-				checked={showErrors}
-				onClick={() => {
-					toggleShowErrors()
-				}
-			}/>
-		</label>
+		<div className="showErrorToggle">
+			<label htmlFor="show-errors">Show Errors
+				<input 
+					type="checkbox" 
+					name="show-errors"
+					checked={showErrors}
+					onClick={() => {
+						toggleShowErrors()
+					}
+				}/>
+			</label>
+		</div>
 	)
 }
 
