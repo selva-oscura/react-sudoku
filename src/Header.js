6c8aefdd4d899550546c596c4b5d90df7c5d050d
Carol St.Louis <1.selva.oscura@gmail.com>
2016-12-17 01:32:16 -0800

    added MenuButton Component to Header Component and passed toggleShowMenu between App and MenuButton

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import MenuButton from './MenuButton';
 import './Header.css';
 
-const Header = () => (
+const Header = ({displayMenu, toggleShowMenu}) => (
   <div className="header">
+  	<MenuButton 
+  		displayMenu={displayMenu}
+  		toggleShowMenu={toggleShowMenu}
+  	/>
     <h2>Sudoku</h2>
   </div>
 );
