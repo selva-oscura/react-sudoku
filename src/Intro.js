689b8e55b734c36c74348d881a888859b3830ff1
Carol St.Louis <1.selva.oscura@gmail.com>
2016-12-16 23:43:58 -0800

    added text for Intro Component

--- src/Intro.js
+++ src/Intro.js
@@ -4,13 +4,10 @@ import './Intro.css';
 const Intro = () => (
 	<div className="intro">
     <p className="App-intro">
-      Will eventually want to display directions here for
+      To select a square, please click on it with your mouse/touchpad, tap on it on your screen, or use the arrow keys to navigate to the square.
     </p>
-    <ol> 
-      <li>basic play</li>
-      <li>setting and changing a square</li>
-      <li>setting and unsetting pencil marks</li>
-    </ol>
+    <p>Once you have selected your target square, to pick a number for that square, type the number (1-9) on your keyboard or tap/click the dark-coloured circle corresponding to the number you want.</p>
+    <p>To make notes ('pencil marks'), select your target square and type shift+number (1-9) or tap/click the ligh-coloured circle corresponding to the number(s) you want.</p>
 	</div>
 );
 
