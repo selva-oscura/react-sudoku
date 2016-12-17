03dbb84ca0d19b48ff45ebbd076a54e83e5bcc37
Carol St.Louis <1.selva.oscura@gmail.com>
2016-12-17 00:28:43 -0800

    reordered/reworded text; added surrounding div; added event handlers to trigger dismissIntro in App

--- src/Intro.js
+++ src/Intro.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import './Intro.css';
 
-const Intro = () => (
-	<div className="intro">
-    <p className="App-intro">
-      To select a square, please click on it with your mouse/touchpad, tap on it on your screen, or use the arrow keys to navigate to the square.
-    </p>
-    <p>Once you have selected your target square, to pick a number for that square, type the number (1-9) on your keyboard or tap/click the dark-coloured circle corresponding to the number you want.</p>
-    <p>To make notes ('pencil marks'), select your target square and type shift+number (1-9) or tap/click the ligh-coloured circle corresponding to the number(s) you want.</p>
+const Intro = ({dismissIntro}) => (
+	<div className="intro"
+      onClick={() => {
+        dismissIntro();
+      }
+    }>
+    <div className="intro-body">
+      <button
+        onClick={() => {
+          dismissIntro();
+        }
+      }>
+        X
+      </button>
+      <p className="instructions">Instructions:</p>
+      <p>
+        To select a square, please<br />
+        <ul>
+          <li>tap the square on your screen,</li>
+          <li>click on the square with your mouse/touchpad, or</li>
+          <li>use the arrow keys to navigate to the square</li>
+        </ul>
+      Once you have selected your target square, to pick a number for that square,<br />
+        <ul>
+          <li>tap/click the dark-coloured circle corresponding to the number you want or</li>
+          <li>type the number (1-9) on your keyboard</li>
+        </ul>
+      To make notes ('pencil marks'), select your target square and <br />
+        <ul>
+          <li>tap/click the light-coloured circle corresponding to the number(s) you want or</li>
+          <li>type shift+number (1-9)</li>
+        </ul>
+      </p>
+    </div>
 	</div>
 );
 
