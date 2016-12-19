ee316556d6041789ece96c5d40a68c54c855f831
Carol St.Louis <1.selva.oscura@gmail.com>
2016-12-18 21:31:06 -0800

    created Menu Component to trigger New Game, Restart Game, and Show Instructions

---
+++ src/Menu.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Menu.css';
+
+const Menu = ({toggleShowMenu, newGame, restartGame, showIntro}) => (
+	<div className="menu">
+		<div className="modal"
+      onClick={() => {
+        toggleShowMenu();
+      }
+    }>
+			<div className="menu-body">
+				<button 
+					onClick={() => {
+						newGame()
+					}
+				}>
+					New Game
+				</button>
+				<button 
+					onClick={() => {
+						restartGame()
+					}
+				}>
+					Restart Game
+				</button>
+				<button 
+					onClick={() => {
+						showIntro()
+					}
+				}>
+					Show Instructions
+				</button>
+			</div>
+		</div>
+	</div>
+);
+
+
+export default Menu;
