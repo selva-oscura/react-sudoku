f8267f338c5774861fea216d8093eac3f7a63ce6
Carol St.Louis <1.selva.oscura@gmail.com>
2016-12-18 19:27:43 -0800

    reordered Sudoku title and menu button

--- src/Header.js
+++ src/Header.js
@@ -4,10 +4,10 @@ import './Header.css';
 
 const Header = ({toggleShowMenu}) => (
   <div className="header">
+    <h2>Sudoku</h2>
   	<MenuButton 
   		toggleShowMenu={toggleShowMenu}
   	/>
-    <h2>Sudoku</h2>
   </div>
 );
 
