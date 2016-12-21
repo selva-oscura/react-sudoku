7ba1205923e13630b78095c14c57014977e60b1e
Carol St.Louis <1.selva.oscura@gmail.com>
2016-12-20 18:18:11 -0800

    removed ul elements from within p

--- src/Intro.js
+++ src/Intro.js
@@ -17,23 +17,23 @@ const Intro = ({dismissIntro}) => (
       </button>
       <p className="instructions">Instructions:</p>
       <p>
-        To select a square, please<br />
-        <ul>
-          <li>tap the square on your screen,</li>
-          <li>click on the square with your mouse/touchpad, or</li>
-          <li>use the arrow keys to navigate to the square</li>
-        </ul>
-      Once you have selected your target square, to pick a number for that square,<br />
-        <ul>
-          <li>tap/click the dark-coloured circle corresponding to the number you want or</li>
-          <li>type the number (1-9) on your keyboard</li>
-        </ul>
-      To make notes ('pencil marks'), select your target square and <br />
-        <ul>
-          <li>tap/click the light-coloured circle corresponding to the number(s) you want or</li>
-          <li>type shift+number (1-9)</li>
-        </ul>
+        To select a square, please
       </p>
+      <ul>
+        <li>tap the square on your screen,</li>
+        <li>click on the square with your mouse/touchpad, or</li>
+        <li>use the arrow keys to navigate to the square</li>
+      </ul>
+      <p>Once you have selected your target square, to pick a number for that square,</p>
+      <ul>
+        <li>tap/click the dark-coloured circle corresponding to the number you want or</li>
+        <li>type the number (1-9) on your keyboard</li>
+      </ul>
+      <p>To make notes ('pencil marks'), select your target square and </p>
+      <ul>
+        <li>tap/click the light-coloured circle corresponding to the number(s) you want or</li>
+        <li>type shift+number (1-9)</li>
+      </ul>
     </div>
 	</div>
 );
