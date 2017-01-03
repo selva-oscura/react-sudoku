f17e0345b7514cbafde5d14fbcaf9374ee291dff
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-02 19:00:06 -0800

    corrected keyup listener function to not output error message in response to shift key

--- src/App.js
+++ src/App.js
@@ -525,7 +525,6 @@ const App = React.createClass({
           let message = "Please click only 1-9 to make your choice (or clear your choice by hitting 0 or X).";
           this.setState({message});
         }
-
       }else{
         if(keyCode===48 || keyCode===88){
           this.updateInkMark("X");
@@ -543,7 +542,7 @@ const App = React.createClass({
             selectedSquare[0] = (selectedSquare[0]<8) ? selectedSquare[0]+=1 : 8;
           }
           this.selectSquare(selectedSquare[0], selectedSquare[1]);
-        }else{
+        }else if(!isShift){
           let message = "Please click only 1-9 to make your choice (or clear your choice by hitting 0 or X).";
           this.setState({message});
         }
