17d557ede844ff0af2a80e5a426c8b6428ae1bc4
Carol St.Louis <1.selva.oscura@gmail.com>
2017-04-06 22:30:01 -0700

    minor text and bolding changes to instructions in Intro component

--- src/Intro.js
+++ src/Intro.js
@@ -20,18 +20,18 @@ const Intro = ({dismissIntro}) => (
         To select a square, please
       </p>
       <ul>
-        <li>tap the square on your screen,</li>
-        <li>click on the square with your mouse/touchpad, or</li>
+        <li>tap the square on your screen <b>or</b></li>
+        <li>click on the square with your mouse/touchpad <b>or</b></li>
         <li>use the arrow keys to navigate to the square</li>
       </ul>
       <p>Once you have selected your target square, to pick a number for that square,</p>
       <ul>
-        <li>tap/click the dark-coloured circle corresponding to the number you want or</li>
+        <li>tap/click the dark-coloured circle corresponding to the number you want <b>or</b></li>
         <li>type the number (1-9) on your keyboard</li>
       </ul>
       <p>To make notes ('pencil marks'), select your target square and </p>
       <ul>
-        <li>tap/click the light-coloured circle corresponding to the number(s) you want or</li>
+        <li>tap/click the light-coloured circle corresponding to the number(s) you want <b>or</b></li>
         <li>type shift+number (1-9)</li>
       </ul>
     </div>
