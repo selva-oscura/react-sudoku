4878364219fe90645858b729f2d575be6270a493
Carol St.Louis <1.selva.oscura@gmail.com>
2017-04-18 23:11:27 -0700

    moved Timer Component to left; added PauseGame Component to right;

--- src/GamesInfo.js
+++ src/GamesInfo.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import Timer from './Timer';
 import History from './History';
+import PauseGame from './PauseGame';
 import ShowErrorToggle from './ShowErrorToggle';
 import './GamesInfo.css';
 
-const GamesInfo = ({timer, scores, showErrors, toggleShowErrors}) => (
+const GamesInfo = ({timer, scores, pausedGame, pausedGameToggle, showErrors, toggleShowErrors}) => (
 	<div className="gamesInfo">
 		<div>		
+			<Timer 
+				timer={timer}
+			/>
 			<History 
 				scores={scores}
 			/>
 		</div>
-		<div>		
-			<Timer 
-				timer={timer}
+		<div>
+			<PauseGame 
+				pausedGame={pausedGame}
+				pausedGameToggle={pausedGameToggle}
 			/>
 			<ShowErrorToggle 
 			  showErrors={showErrors}
