09c97f49857ab468e4f4e850124a05ac7ca73b5d
Carol St.Louis <1.selva.oscura@gmail.com>
2017-07-09 18:40:27 -0700

    converted 'row-divider-*' className to 'box-*' className

--- src/Square.js
+++ src/Square.js
@@ -10,18 +10,18 @@ const Square = ({rowNumber, colNumber, square, selectSquare, showErrors}) => {
 	}else if(rowNumber===9){
 		squareStyle += " board-bottom"
 	}else if(rowNumber===3 || rowNumber===6){
-		squareStyle += " row-divider-bottom";
+		squareStyle += " box-bottom";
 	}else if(rowNumber===4 || rowNumber===7){
-		squareStyle += " row-divider-top";
+		squareStyle += " box-top";
 	}
 	if(colNumber===1){
 		squareStyle += " board-left";
 	}else if(colNumber===9){
 		squareStyle += " board-right"
 	}else if(colNumber===3 || colNumber===6){
-		squareStyle += " row-divider-right";
+		squareStyle += " box-right";
 	}else if(colNumber===4 || colNumber===7){
-		squareStyle += " row-divider-left";
+		squareStyle += " box-left";
 	}
 	if(square.selected){
 		squareStyle += " selected";
