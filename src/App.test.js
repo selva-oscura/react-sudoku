971cc6cb8e2686bf6772ea5f9e9ee493e7508d43
Carol St.Louis <1.selva.oscura@gmail.com>
2017-07-10 21:00:56 -0700

    added basic App and Header test; added mock for localStorage;

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import Header from './Header';
 
 it('App renders without crashing', () => {
+	var mock = (function() {
+	  var store = {};
+	  return {
+	    getItem: function(key) {
+	      return store[key];
+	    },
+	    setItem: function(key, value) {
+	      store[key] = value.toString();
+	    },
+	    clear: function() {
+	      store = {};
+	    }
+	  };
+	})();
+	Object.defineProperty(window, 'localStorage', { value: mock });
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
 
+it('Header renders without crashing', () => {
+	const div = document.createElement('div');
+	ReactDOM.render(<Header />, div)
+});
